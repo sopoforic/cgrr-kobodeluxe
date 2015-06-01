ef1d74af43ab236b7fd5bf025935b7c286ef45f3
Tracy Poff <tracy.poff@gmail.com>
2015-06-01 05:53:18 -0400

    Remove unused import.

--- kobodeluxe.py
+++ kobodeluxe.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with CGRR.  If not, see <http://www.gnu.org/licenses/>.
 """Parses Kobo Deluxe data."""
-import logging
 import os
 
 import cgrr
